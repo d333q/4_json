4685c8318c8c09db7108c13756b518e009a5acc5
d333q <d33q@yandex.ru>
2016-10-01 10:56:15 +0000

    corrected

--- pprint_json.py
+++ pprint_json.py
@@ -2,16 +2,19 @@ import json
 from pprint import pprint
 import os
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, 'r') as json_content:
         return json.load(json_content)
 
+
 def pretty_print_json(filepath):
     return pprint(data)
 
 
 if __name__ == '__main__':
-    data = load_data(input('Введите название файла со списком московских алкогольных магазинов '))
+    data = load_data(input(
+        'Введите название файла со списком московских алкогольных магазинов '))
     pretty_print_json(data)
